774675f75d4fc85cf452449f390e991fd0fa644f
Mikhail Sakhnyuk <its545@yandex.ru>
2019-07-05 23:52:08 +0700

    Update package.json

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,5 @@ import exchanges from "./exchanges";
 const socket = new exchanges.binance();
 
 socket.onTrade("BTC/USDT", (data: any) =>
-  console.log(
-    data.exchange,
-    data.price,
-    `${new Date(data.timestamp).getHours()}:${new Date(
-      data.timestamp
-    ).getMinutes()}:${new Date(data.timestamp).getSeconds()}`
-  )
+  console.log(data.exchange, data.price)
 );
