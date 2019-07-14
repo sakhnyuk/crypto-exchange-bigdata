438569b3a376768757dee6617debc1333446b1e9
Mikhail Sakhnyuk <its545@yandex.ru>
2019-07-14 14:09:46 +0700

    add DB postgres on Docker

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,19 @@ import exchanges from "./exchanges";
 const socket = new exchanges.Binance();
 const socket2 = new exchanges.Bitmex();
 
-socket.onTrade("BTC/USDT", (data: any) =>
-  console.log(data.exchange, data.price)
-);
+socket.onTrade("BTC/USDT", (data: any) => {
+  console.log(data);
+});
 
-socket2.onTrade("XBT/USD", (data: any) =>
-  console.log(data.exchange, data.price)
-);
+// socket2.onTrade("XBT/USD", (data: any) => {
+//   // console.log(data.exchange, data.price)
+//   count = count + 1;
+
+//   console.log(count);
+// });
+
+for (const exch in socket) {
+  if (socket.hasOwnProperty(exch)) {
+    const session = socket[exch];
+  }
+}
